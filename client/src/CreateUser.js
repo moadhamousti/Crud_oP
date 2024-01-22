c29c707be833ce9899dd74300c253391181d6d5a
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-22 15:05:23 +0100

    Update CreateUser.js

--- client/src/CreateUser.js
+++ client/src/CreateUser.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios' 
 import { useNavigate } from 'react-router-dom'
 
-
+ 
 export default function CreateUser() {
     const[firstname, setFirstname] =useState()
     const[lastname, setLastname] =useState()
