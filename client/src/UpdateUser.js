89c9046fdac1d6c89457f1324f0ba7910c4b3207
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2024-01-22 15:03:43 +0100

    Update UpdateUser.js

--- client/src/UpdateUser.js
+++ client/src/UpdateUser.js
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"; 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import photo from './images/photo.jpg';
